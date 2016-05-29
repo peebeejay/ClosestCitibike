494bb34bb44499f6c4b08c5ca1903abf919ac02f
Rohan Kshirsagar <rohank12345@gmail.com>
2016-05-28 23:06:57 -0400

    fixing indent issue

--- app.py
+++ app.py
@@ -7,18 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "Hello World!"
+        return "Hello World!"
 
 
 @app.route('/citibike')
 def citibike():
-	r = requests.get('https://gbfs.citibikenyc.com/gbfs/gbfs.json')
-	d = r.json()
-    list_keys = d.keys()
-    print(type(d['data']))
-    print(d['data']['en']['feeds'][0])
-    return pprint.pformat(d, indent=4)
+        r = requests.get('https://gbfs.citibikenyc.com/gbfs/gbfs.json')
+        d = r.json()
+        list_keys = d.keys()
+        print(type(d['data']))
+        print(d['data']['en']['feeds'][0])
+        return pprint.pformat(d, indent=4)
 
 
 if __name__ == '__main__':
-	app.run()
+        app.run()
