ecc93cd54de2f621421aa51bc87a9ee8e01c123a
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2016-05-28 23:00:29 -0400

    close #1

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import pprint
+import requests
 
 
 app = Flask(__name__)
@@ -6,6 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return "Hello World!"
+
+
+@app.route('/citibike')
+def citibike():
+	r = requests.get('https://gbfs.citibikenyc.com/gbfs/gbfs.json')
+
+    d = r.json()
+    list_keys = d.keys()
+    print(type(d['data']))
+    print(d['data']['en']['feeds'][0])
+    return pprint.pformat(d, indent=4)
 
 
 if __name__ == '__main__':
