585c5c673483a2aed3769a2a92c3800d1a12e519
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2016-05-28 22:50:35 -0400

    renamed app1 to app in app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
 
-app1 = Flask(__name__)
+app = Flask(__name__)
 
-@app1.route('/')
+@app.route('/')
 def hello_world():
 	return "Hello World!"
 
 
 if __name__ == '__main__':
-	app1.run()
+	app.run()
