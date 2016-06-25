ce14a19be5d55bbd50ad73af871c6e9270b1034e
Rohan Kshirsagar <rohank12345@gmail.com>
2016-06-24 23:48:22 -0400

    added google maps integration. closes #8.

--- static/js/citibike.js
+++ static/js/citibike.js
@@ -2,59 +2,126 @@
 $("#bikebutton").click(function(){
     console.log("bike button");
     document.getElementById("bikes").style.display = "block";
-    document.getElementById("parking").style.display = "none";    
+    document.getElementById("parking").style.display = "none";
+    toggleVisible(bikeMarkers, parkMarkers);
 
 });
 $("#parkbutton").click(function(){
     console.log("park button");
     document.getElementById("parking").style.display = "block";
     document.getElementById("bikes").style.display = "none";    
-
+    toggleVisible(parkMarkers, bikeMarkers);
 });
+
+var map;
+var bikeMarkers = [];
+var parkMarkers = [];
+function initMap(latitude, longitude) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: latitude, lng: longitude},
+        zoom: 15
+    });
+
+    var marker = new google.maps.Marker({
+	position: map.getCenter(),
+	icon: {
+	    path: google.maps.SymbolPath.CIRCLE,
+	    scale: 4,
+	    fillColor: "#3300CC",
+	    strokeColor: "#3300CC",
+	},
+	map: map
+    });
+}
+
+function toggleVisible(enableMarkers, disableMarkers)
+{
+    len = disableMarkers.length;
+    for (var i = 0; i < len; ++i)
+    {
+	console.log(i, enableMarkers[i]);
+
+	enableMarkers[i].setVisible(true);
+	disableMarkers[i].setVisible(false);	
+    }
+}
+
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
+function addMarker(latitude, longitude, markers) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    console.log(typeof(lat));
+    console.log(typeof(longitude));
+    var myLatlng = new google.maps.LatLng(latitude,longitude);
+    var marker = new google.maps.Marker({
+	position: myLatlng,
+	label: labels[labelIndex++ % labels.length],
+	map: map
+    });
+    markers.push(marker);
+    return  labels[labelIndex-1];
+}
+
+
 
 var x = document.getElementById("p_coordinates");
         var s = document.getElementById("stations");
         var lat = "", lon = "";
 
         function get_location(){
-        	if (navigator && navigator.geolocation) {
-        		navigator.geolocation.getCurrentPosition(showPosition, displayError);
+            if (navigator && navigator.geolocation) {
+		console.log("Getting geolocation")
+        	navigator.geolocation.getCurrentPosition(showPosition, displayError);
         	} 
         	else {
             	console.log('Geolocation is not supported');
         	}
         }
 
-        function showPosition(position) {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            x.innerHTML = "<b>Latitude: </b>" + lat + "<br><b>Longitude: </b>" + lon;
-            send_geo();
-        }
-     
-        function send_geo(){
-            console.log(lat, lon);       
-            $.getJSON($SCRIPT_ROOT + '/receive_coord', {lat: lat, lon: lon}, 
-                      function(data) {
-			  var i = 0;
-			  $.each(data.result, function() {
-			      var ul0= $('<ul class="list-group">');
-			      console.log(this.length)
-			      $.each(this, function() {
-				      ul0.append($('<li>',
-						   {text: this.name,
-						    class: 'list-group-item'})); // TODO: Add rest of content here
-			      });
-			      if (i == 0)
-				  $("#bikes").append(ul0);
-			      else
-				  $("#parking").append(ul0);
+function showPosition(position) {
+    console.log("Geolocation coordinates acquired");
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    initMap(lat,lon)
+    send_geo();
+}
 
-			      i++;
+function send_geo(){
+    console.log(lat, lon);       
+    $.getJSON($SCRIPT_ROOT + '/receive_coord', {lat: lat, lon: lon}, 
+              function(data) {
+		  var i = 0;
+		  $.each(data.result, function() {
+		      var ul0= $('<ul class="list-group">');
+		      console.log(this.length)
+		      $.each(this, function() {
 
-			      
-			  });
+			  let letter = addMarker(this.lat, this.lon, i ? parkMarkers: bikeMarkers);
+			  ul0.append($('<li>',
+				       {text: letter + ": " + this.name,
+					class: 'list-group-item'})); // TODO: Add rest of content here
+		      });
+		      if (i == 0)
+			  $("#bikes").append(ul0);
+		      else
+			  $("#parking").append(ul0);
 
+		      i++;
+
+		      
+		  });
+		  toggleVisible(bikeMarkers, parkMarkers);
+              }
+             );
+}
+
+function displayError(){
+    x.innerHTML = "Geolocation unavailable";
+}
+
+window.onload = get_location;  
 			      // var li0= $('<li>', {text: this.title});
 			      // var ul1= $('<ul>');
 			      // $.each(data.result, function() {
@@ -80,15 +147,9 @@ var x = document.getElementById("p_coordinates");
 			  // }
 			  // console.log(str1);
 			  // stations.innerHTML = str1;
-                      }
-            );
-        }
 
-        function displayError(){
-        	x.innerHTML = "Geolocation unavailable";
-        }
 
-        window.onload = get_location;  
+       
 
         /* //The function below is usable with Flask: request.args.get('key', default value, type=type)
         function send_geo(){ 
