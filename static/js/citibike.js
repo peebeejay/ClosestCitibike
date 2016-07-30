1cfacb6c61d322ddf15b75920e201a647440ec07
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2016-07-29 22:28:30 -0400

    modified google maps theme

--- static/js/citibike.js
+++ static/js/citibike.js
@@ -70,13 +70,13 @@ function initMap(latitude, longitude) {
         zoom: 15, 
         scrollwheel: false,
 
-    styles: [   {   "stylers":[ {"visibility":"on"},    {"saturation":-100},    {"gamma":0.54}  ]   },{ "featureType":"road",   "elementType":"labels.icon",    
-        "stylers":[ {"visibility":"off"}    ]   },{ "featureType":"water",  "stylers":[ {"color":"#4d4946"} ]   },{ "featureType":"poi",    "elementType":"labels.icon",
-        "stylers":[ {"visibility":"off"}    ]   },{ "featureType":"poi",    "elementType":"labels.text",    "stylers":[ {"visibility":"simplified"} ]   },
-        { "featureType":"road",   "elementType":"geometry.fill",  "stylers":[ {"color":"#ffffff"} ]   },{ "featureType":"road.local", "elementType":"labels.text",
-        "stylers":[ {"visibility":"simplified"} ]   },{ "featureType":"water",  "elementType":"labels.text.fill",   "stylers":[ {"color":"#ffffff"} ]   },
-        { "featureType":"transit.line",   "elementType":"geometry",   "stylers":[ {"gamma":0.48}  ]   },{ "featureType":"transit.station",    
-        "elementType":"labels.icon",    "stylers":[ {"visibility":"off"}    ]   },{ "featureType":"road",   "elementType":"geometry.stroke",   
+        styles: [   {   "stylers":[ {"visibility":"on"},    {"saturation":-100},    {"gamma":0.54}  ]   },{ "featureType":"road",   "elementType":"labels.icon",    
+            "stylers":[ {"visibility":"off"}    ]   },{ "featureType":"water",  "stylers":[ {"color":"#4d4946"} ]   },{ "featureType":"poi",    "elementType":"labels.icon",
+            "stylers":[ {"visibility":"off"}    ]   },{ "featureType":"poi",    "elementType":"labels.text",    "stylers":[ {"visibility":"simplified"} ]   },
+            { "featureType":"road",   "elementType":"geometry.fill",  "stylers":[ {"color":"#ffffff"} ]   },{ "featureType":"road.local", "elementType":"labels.text",
+            "stylers":[ {"visibility":"simplified"} ]   },{ "featureType":"water",  "elementType":"labels.text.fill",   "stylers":[ {"color":"#ffffff"} ]   },
+            { "featureType":"transit.line",   "elementType":"geometry",   "stylers":[ {"gamma":0.48}  ]   },{ "featureType":"transit.station",    
+            "elementType":"labels.icon",    "stylers":[ {"visibility":"off"}    ]   },{ "featureType":"road",   "elementType":"geometry.stroke",   
         "stylers":[ {"gamma":7.18}  ]   }   ]
         
     });
