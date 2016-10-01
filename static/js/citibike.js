899ec7acc62ef71a6058953643befc9c1f5b8ec5
Jay Puntham-Baker <jaypuntambekar@gmail.com>
2016-10-01 00:03:27 -0400

    moved numerous functions to site.js

--- static/js/citibike.js
+++ static/js/citibike.js
@@ -1,49 +1,13 @@
-//
-//
-//
-
-$("#bikebutton").click(function(){
-    console.log("bike button");
-    document.getElementById("bikes").style.display = "block";
-    document.getElementById("parking").style.display = "none";
-    toggleVisible(bikeMarkers, parkMarkers);
-
-});
-$("#parkbutton").click(function(){
-    console.log("park button");
-    document.getElementById("parking").style.display = "block";
-    document.getElementById("bikes").style.display = "none";    
-    toggleVisible(parkMarkers, bikeMarkers);
-});
 
 var map;
 var bikeMarkers = [];
 var parkMarkers = [];
-var bar = new ProgressBar.Circle("#progress", {
-  strokeWidth: 6,
-  easing: 'easeInOut',
-  duration: 1000,
-  color: '#000000',
-  trailColor: '#30A4EF',
-  trailWidth: 1,
-  svgStyle: null
-});
-bar.animate(1)
-setTimeout(function() {
-    bar.animate(0.25);
-}, 1000);
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+var x = document.getElementById("p_coordinates");
+var s = document.getElementById("stations");
+var lat = "", lon = "";
 
-setTimeout(function() {
-    bar.animate(1.0);
-}, 2000);
-
-setTimeout(function() {
-    bar.animate(.5);
-}, 3000);
-
-setTimeout(function() {
-    bar.animate(1.0);
-}, 4000);
 
 $("#address").keyup(function(event){
     if(event.keyCode == 13){
@@ -53,11 +17,12 @@ $("#address").keyup(function(event){
 	var geocoder = new google.maps.Geocoder();
 	geocoder.geocode( { 'address': address}, function(results, status) {
 	    if (status == google.maps.GeocoderStatus.OK) {
-		lat = results[0].geometry.location.lat();
-		lon = results[0].geometry.location.lng();
-		refreshPosition();
-	    } else {
-		alert("Geocode was not successful for the following reason: " + status);
+            lat = results[0].geometry.location.lat();
+            lon = results[0].geometry.location.lng();
+            refreshPosition();
+	    } 
+        else {
+		  alert("Geocode was not successful for the following reason: " + status);
 	    }
 	});
     }
@@ -65,14 +30,12 @@ $("#address").keyup(function(event){
 
 
 function initMap(latitude, longitude) {
-
     var customMapTypeId = 'custom_style';
-
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: latitude, lng: longitude},
         zoom: 15, 
         scrollwheel: false,
-
+        streetViewControl: false,
         styles: [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},
             {"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},
             {"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},
@@ -80,9 +43,7 @@ function initMap(latitude, longitude) {
             {"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},
             {"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}]       
     });
-
     
-
     var marker = new google.maps.Marker({
     	position: map.getCenter(),
     	icon: {
@@ -104,37 +65,26 @@ function initMap(latitude, longitude) {
           infowindow.open(map, marker);
         });
 
-
-  
-    
-
     document.getElementById('map').style.display="block";
     document.getElementById('progress').style.display="none";
 }
 
-function toggleVisible(enableMarkers, disableMarkers)
-{
+function toggleVisible(enableMarkers, disableMarkers){
     len = disableMarkers.length;
-    for (var i = 0; i < len; ++i)
-    {
-	console.log(i, enableMarkers[i]);
-	enableMarkers[i].setVisible(true);
-	disableMarkers[i].setVisible(false);	
+    for (var i = 0; i < len; ++i){
+        console.log(i, enableMarkers[i]);
+        enableMarkers[i].setVisible(true);
+        disableMarkers[i].setVisible(false);	
     }
 }
 
-function clearMarkers(markers)
-{
+function clearMarkers(markers){
     len = markers.length;
-    for (var i = 0; i < len; ++i)
-    {
-	markers[i].setMap(null);
+    for (var i = 0; i < len; ++i){
+        markers[i].setMap(null);
     }
     markers = []
 }
-
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var labelIndex = 0;
 
 function addMarker(address, latitude, longitude, markers) {
     // Add the marker at the clicked location, and add the next-available label
@@ -144,63 +94,29 @@ function addMarker(address, latitude, longitude, markers) {
     console.log(typeof(longitude));
     var myLatlng = new google.maps.LatLng(latitude,longitude);
     var marker = new google.maps.Marker({
-	position: myLatlng,
-	label: labels[labelIndex++ % labels.length],
-	map: map
+        position: myLatlng,
+        label: labels[labelIndex++ % labels.length],
+        map: map
     });
 
     var contentString = address.num_bikes_available +' available bikes at '+address.name;
-    var contentString2 = "<div style=color:#2C317A><b>"+ address.name +"</b></div> <p style='display:inline'> Available Bikes: " +address.num_bikes_available+ '</p> <p>Available Docks: '+address.num_docks_available+"</p>";
+    var contentString2 = "<div style=color:#2C317A><b>"+ address.name +"</b></div> <p style='display:inline'> Bikes: " +address.num_bikes_available+ '</p> <p> Docks: '+address.num_docks_available+"</p>";
     
     var infowindow = new google.maps.InfoWindow({
           content: contentString2
-        });
+    });
     prev_infowindow = false;
-     marker.addListener('click', function() {
+    marker.addListener('click', function() {
         if(prev_infowindow) {
             prev_infowindow.close();
         }
         prev_infowindow = infowindow;
         infowindow.open(map, marker);
-        });
+    });
 
     markers.push(marker);
     return  labels[labelIndex-1];
 }
-
-String.prototype.ljust = function( width, padding ) {
-    padding = padding || " ";
-    padding = padding.substr( 0, 1 );
-    if( this.length < width )
-        return this + padding.repeat( width - this.length );
-    else
-        return this;
-}
-String.prototype.rjust = function( width, padding ) {
-    padding = padding || " ";
-    padding = padding.substr( 0, 1 );
-    if( this.length < width )
-        return padding.repeat( width - this.length ) + this;
-    else
-        return this;
-}
-String.prototype.center = function( width, padding ) {
-    padding = padding || " ";
-    padding = padding.substr( 0, 1 );
-    if( this.length < width ) {
-        var len     = width - this.length;
-        var remain  = ( len % 2 == 0 ) ? "" : padding;
-        var pads    = padding.repeat( parseInt( len / 2 ) );
-        return pads + this + pads + remain;
-    }
-    else
-        return this;
-}
-
-
-var x = document.getElementById("p_coordinates");
-var s = document.getElementById("stations");
-var lat = "", lon = "";
 
 function get_location(){
     if (navigator && navigator.geolocation) {
@@ -220,8 +136,7 @@ function showPosition(position) {
     send_geo();
 }
 
-function refreshPosition()
-{
+function refreshPosition(){
     clearMarkers(parkMarkers);
     clearMarkers(bikeMarkers);
     $("#bikes").empty();
@@ -244,7 +159,6 @@ function send_geo(){
                     var table_string = (letter + ")").ljust(3) + 
                         ("B: " + this.num_bikes_available + "  D: " + this.num_docks_available).ljust(12) + " - " + this.name;
                     console.log(table_string);
-                    // text: letter.ljust(10) + ": " + this.name + "- B:" + this.num_bikes_available + "  D:" + this.num_docks_available,
 
                     ul0.append($('<li>',
                         {text: table_string,
@@ -267,64 +181,3 @@ function displayError(){
 }
 
 window.onload = get_location;  
-			      // var li0= $('<li>', {text: this.title});
-			      // var ul1= $('<ul>');
-			      // $.each(data.result, function() {
-			      // 	  ul1.append($('<li>', {id: 'foo_'+this.title})
-			      // 		     .append($('<a>', {href: this.link, text: this.text})
-			      // 			     .append($('<strong>', {text: this.data0}))
-			      // 			     .append($('<span>', {text: this.data1}))
-			      // 			     .append($('<em>', {text: this.data2}))
-			      // 			    )
-			      // 		    );
-			      // });
-			      // li0.append(ul1);
-			      // ul0.append(li0);
-			  // });
-			  // var str1 = "";
-			  // var headers = ["<b>Bikes Available - 2 minimum</b>", "<b>Docks Available - 2 minimum</b>"];
-			  // for (i=0; i<data.result.length; i++){
-                    	  //     str1 += headers[i] + "<br>";
-                    	  //     for (j=0; j<data.result[i].length; j++){
-                    	  // 	  str1 += ("<b>" + (j+1) + ": " + data.result[i][j].name + "</b> |||  Distance: " + ((data.result[i][j].magnitude)*69).toFixed(2) + " mi  || Bikes: " + data.result[i][j].num_bikes_available + " |  Docks: " + data.result[i][j].num_docks_available+ "<br>");
-                    	  //     }
-                    	  //     str1 += "<br><br>";                   	
-			  // }
-			  // console.log(str1);
-			  // stations.innerHTML = str1;
-
-
-       
-
-        /* //The function below is usable with Flask: request.args.get('key', default value, type=type)
-        function send_geo(){ 
-            $.getJSON($SCRIPT_ROOT + '/receive_coord', {
-                lat: lat,
-                lon: lon
-                }, 
-
-                function(data) {
-                    //$("#result").text(data.result);
-                    console.log(data.result);
-            });
-        }*/ 
-
-        /* // The function below is an outdated version of send_geo that uses an AJAX POST method to send geolocation coordinates to the server
-           // To use this function, use the following in Flask: lat = request.json['lat']  ; # lon = request.json['lon']
-        	function send_geo(){
-            console.log(lat);
-            console.log(lon);
-
-            $.ajax({
-            url: ($SCRIPT_ROOT + '/receive_coord'),
-            data: JSON.stringify({"lat": lat, "lon": lon}, null, '\t'),
-            contentType: 'application/json;charset=UTF-8',
-            type: "POST",
-            success: function(response) {
-                console.log("success:");
-            },
-            error: function(error) {
-                console.log("error");
-            }
-            });
-        }*/
